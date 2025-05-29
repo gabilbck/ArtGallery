67cfc8163f35bd18f9cf8c5256f5c65b72f67c6a
gabilbck <gabrielieduardalembeck@gmail.com>
2025-05-29 12:08:55 -0300

    iniciando testes suporte

--- ArtGallery/test/unitarios/suporte.test.js
+++ ArtGallery/test/unitarios/suporte.test.js
@@ -1,1 +1,31 @@
-// Teste
+const request = require('supertest');
+const express = require('express');
+const session = require('express-session');
+const suporteRouter = require("../../routes/suporte");
+
+jest.mock("../../banco", () => ({
+  inserirSuporte: jest.fn(),
+}));
+
+const banco = require("../../banco");
+
+function createApp() {
+  const app = express();
+  app.use(express.json());
+  app.use((req, res, next) => {
+    res.render = (view, data) => res.json({ view, data });
+    next();
+  });
+  app.use('/supote', suporteRouter);
+  return app;
+}
+
+describe('Rotas /suporte', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  //Testa se foi enviado e retorna sucesso
+  //Testa se o usuário envia sem ter dados e retorna erro
+  //Testa se não dá erro se o usuário sair da página antes de enviar
+
+  
+});
